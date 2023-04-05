940f391742424b6d1cde34288451d8451e61ab65
Veronica Makhvits <nikahendry@gmail.com>
2023-04-05 10:11:14 +0300

    fix header

--- src/components/HeaderTop.tsx
+++ src/components/HeaderTop.tsx
@@ -1,41 +1,39 @@
-import '../sass/header_top.scss';
+import header from '../sass/header_top.module.scss';
 import vector from '../assets/img/Vector_location.png';
 import vector2 from '../assets/img/Vector_location2.png';
+import letter from '../assets/img/Vector_letter.png';
 
 function HeaderTop() {
     return (
-        <div className="header">
-            <div className="header_part left">
-                <div className="header_column">
-                    <div className="header_addressBlock">
-                        <div className="addressBlock_icon">
-                            <img className="icon_location" src={vector} alt=""/>
-                            <img className="icon_location2" src={vector2} alt=""/>
-                        </div>
-                        <div className="header_address">
-                            <h1 className="header_text">г. Кокчетав, ул. Ж. Ташенова 129Б</h1>
-                            <h1 className="address_name">(Рынок Восточный)</h1>
-                        </div>
+        <div className={header.header}>
+            <div className={header.columnLeft}>
+                <div className={header.columnLeft_item}>
+                    <div className={header.item_icon}>
+                        <img className={header.icon_1} src={vector} alt=""/>
+                        <img className={header.icon_2} src={vector2} alt=""/>
+                    </div>
+                    <div className={header.item_text}>
+                        <h1><b>г. Кокчетав, ул. Ж. Ташенова 129Б</b></h1>
+                        <h1>(Рынок Восточный)</h1>
                     </div>
                 </div>
-                <div className="header_column">
-                    <div className="header_contacts">
-                        <h1 className="contacts_email_h">opt.sultan@mail.ru</h1>
-                        <h1 className="contacts_text_h">На связи в любое время</h1>
+                <div className={header.columnLeft_item}>
+                    <div className={header.item_icon}>
+                        <img className={header.icon_letter} src={letter} alt=""/>
+                    </div>
+                    <div className={header.item_text}>
+                        <h1><b>opt.sultan@mail.ru</b></h1>
+                        <h1>На связи в любое время</h1>
                     </div>
                 </div>
             </div>
-            <div className="header_part">
-                <div className="header_column">
-                    <div>
-                        <ul className="header_categories">
-                            <li className="header_iteam">О компании</li>
-                            <li className="header_iteam">Доставка и оплата</li>
-                            <li className="header_iteam">Возврат</li>
-                            <li className="header_iteam">Контакты</li>
-                        </ul>
-                    </div>
-                </div>
+            <div className={header.columnRight}>
+                <ul className={header.columnRight_actions}>
+                    <li className={header.actions_item}>О компании</li>
+                    <li className={header.actions_item}>Доставка и оплата</li>
+                    <li className={header.actions_item}>Возврат</li>
+                    <li className={header.actions_item}>Контакты</li>
+                </ul>
             </div>
         </div>
     )
